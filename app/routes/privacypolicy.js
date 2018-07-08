cc0fc27392b588f377197fb0ae4db1100339975c
tslima <lima.tss@gmail.com>
2018-07-07 21:29:55 -0300

    Corrigindo typo em Privacy Police

--- app/routes/privacypolicy.js
+++ app/routes/privacypolicy.js
@@ -1,10 +1,10 @@
 module.exports = function(app){
-  app.get('/privacypolice',function(req,res){
+  app.get('/privacypolicy',function(req,res){
     var login = '';
     if(req.user) {
       login = req.user.nome;
       console.log(login);
     }
-    res.render('privacypolice');
+    res.render('privacypolicy');
   })
 };
